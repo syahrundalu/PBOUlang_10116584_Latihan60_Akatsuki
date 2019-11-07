dbf0df8ac6d87740ae5b88bdf43691d8c25ab303
Muhammad Syahrun Dalu <55743453+syahrundalu@users.noreply.github.com>
2019-11-08 01:10:53 +0700

    Add files via upload

---
+++ Akatsuki.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang_10116584_latihan60_akatsuki;
+
+/**
+ *
+ * @author syhar
+ */
+public class Akatsuki {
+     protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+
+}
